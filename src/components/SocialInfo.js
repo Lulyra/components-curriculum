694fb399349e703bca2195842f32f6a490369b62
Luiz Paulo Oliveira Lyra de Miranda <luiz_paulo6@hotmail.com>
2021-02-13 17:04:11 -0300

    structuring SocialInfo Component

--- src/components/SocialInfo.js
+++ src/components/SocialInfo.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 function SocialInfo(props) {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <GitHubIcon />
+        <p>{props.data.github}</p>
+      </div>
+      <div>
+        <LinkedInIcon />
+        <p>{props.data.linkedIn}</p>
+      </div>
+    </div>
+  );
 }
 
-export default SocialInfo
+export default SocialInfo;
