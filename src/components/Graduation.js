54b810162b5bddba84b06fe124b85da55320adc6
Luiz Paulo Oliveira Lyra de Miranda <luiz_paulo6@hotmail.com>
2021-02-13 17:03:07 -0300

    structuring Graduation Component

--- src/components/Graduation.js
+++ src/components/Graduation.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import nextId from "react-id-generator";
+import SchoolIcon from "@material-ui/icons/School";
 
 function Graduation(props) {
+  const graduationComponents = props.data.map((formation) => {
+    let id = nextId();
     return (
-        <div>
-            
-        </div>
-    )
+      <div key={id}>
+        <h4>{formation.typeOfGraduation}</h4>
+        <h4>{formation.university}</h4>
+        <h4>
+          {formation.startDate} - {formation.endDate}
+        </h4>
+        <h4>{formation.subject}</h4>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div>
+        <SchoolIcon />
+        <h3>Formação</h3>
+      </div>
+      {graduationComponents}
+    </div>
+  );
 }
 
-export default Graduation
+export default Graduation;
