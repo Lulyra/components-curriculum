8d872cc2f67919284b27ed95de939c1cb8f86474
Luiz Paulo Oliveira Lyra de Miranda <luiz_paulo6@hotmail.com>
2021-02-13 15:59:38 -0300

    Create structure of props and componets.

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -1,16 +1,28 @@
 import React from "react";
 import PersonalInfo from "./PersonalInfo";
+import SocialInfo from "./SocialInfo";
+import Introduction from "./Introduction";
+import Graduation from "./Graduation";
+import Experience from "./Experience";
 
 function Curriculum(props) {
   return (
     <div class="grid-container">
       <div class="PersonalInfo">
-        <PersonalInfo />
+        <PersonalInfo data={props.PersonalInfo} />
       </div>
-      <div class="SocialInfo"></div>
-      <div class="Introduction"></div>
-      <div class="Graduation"></div>
-      <div class="Experience"></div>
+      <div class="SocialInfo">
+        <SocialInfo data={props.SocialInfo} />
+      </div>
+      <div class="Introduction">
+        <Introduction data={props.Introduction} />
+      </div>
+      <div class="Graduation">
+        <Graduation data={props.Graduation} />
+      </div>
+      <div class="Experience">
+        <Experience data={props.Experience} />
+      </div>
     </div>
   );
 }
