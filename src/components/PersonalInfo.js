89097cd4c8ec4d55f9c6035365438555f41586bf
Luiz Paulo Oliveira Lyra de Miranda <luiz_paulo6@hotmail.com>
2021-02-13 17:03:53 -0300

    structuring PersonalInfo Component

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -3,22 +3,22 @@ import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import RoomIcon from "@material-ui/icons/Room";
 
-function PersonalInfo() {
+function PersonalInfo(props) {
   return (
     <div className="PersonalInfo">
-      <p className=""></p>
-      <p className="">Engenheiro de Software</p>
+      <h1>{props.data.name}</h1>
+      <h3>{props.data.profession}</h3>
       <div className="Phone">
         <PhoneAndroidIcon />
-        <p className="Phone__value">+55 (21) 99404-6890</p>
+        <p className="Phone__value">{props.data.phone}</p>
       </div>
       <div className="Email">
         <MailOutlineIcon />
-        <p className="Email__value">luiz_paulo6@hotmail.com</p>
+        <p className="Email__value">{props.data.email}</p>
       </div>
       <div className="Location">
         <RoomIcon />
-        <p className="Location__value">Rio de Janeiro, RJ - Brasil</p>
+        <p className="Location__value">{props.data.location}</p>
       </div>
     </div>
   );
