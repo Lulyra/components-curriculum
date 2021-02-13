350b0b24bb0d557eb67fa1f162759c156050ad92
Luiz Paulo Oliveira Lyra de Miranda <luiz_paulo6@hotmail.com>
2021-02-13 17:50:41 -0300

    Add css to SocialInfor Component
    Add some structure

--- src/components/SocialInfo.js
+++ src/components/SocialInfo.js
@@ -1,15 +1,16 @@
 import React from "react";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import "../styles/SocialInfo.css";
 
 function SocialInfo(props) {
   return (
-    <div>
-      <div>
+    <div className="socialInfo__main">
+      <div className="github">
         <GitHubIcon />
         <p>{props.data.github}</p>
       </div>
-      <div>
+      <div className="linkedIn">
         <LinkedInIcon />
         <p>{props.data.linkedIn}</p>
       </div>
